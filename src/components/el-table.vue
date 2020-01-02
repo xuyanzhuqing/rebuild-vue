b7ea577cbe133bcf4e22b1d3f2e71aeea0c0936d
徐存根 <michaelx@k2workflow.com.cn>
2020-01-02 15:12:20 +0800

    '测试jsx语法'

---
+++ src/components/el-table.vue
@@ -0,0 +1,166 @@
+<template>
+  <el-table
+    ref="table"
+    v-bind="$attrs"
+    v-on="$listeners"
+    class="el-table--self"
+    style="width: 100%">
+    <table-column v-for="(val, index) in column" :value="val" :key="index"/>
+    <slot></slot>
+  </el-table>
+</template>
+
+<script>
+export default {
+  // 核心点
+  inheritAttrs: false,
+  props: {
+    // 开启自定义单选
+    radio: {
+      required: false,
+      type: Boolean,
+      default: false
+    },
+    // 单选表头信息
+    radioLabel: {
+      required: false,
+      type: String,
+      default: '请选择'
+    },
+    enableFilters: {
+      required: false,
+      type: Boolean,
+      default: false
+    },
+    // 表头字段过滤
+    filters: {
+      required: false,
+      type: Array,
+      default: () => ([])
+    }
+  },
+  data () {
+    return {
+      radioValue: null,
+      filterField: []
+    }
+  },
+  created () {
+    this.filterField = this.filters.slice(0)
+  },
+  computed: {
+    radioCol () {
+      const me = this
+      const {rowKey} = me.$attrs
+      const col = {
+        label: me.radioLabel,
+        prop: 'radio',
+        render (h, {row, $index}) {
+          return (
+            <el-radio v-model={me.radioValue} label={row[rowKey] || $index} class="no--label" onChange={(v) => me.$emit('radio-change', v)}></el-radio>
+          )
+        }
+      }
+      return col
+    },
+    filterCol () {
+      const me = this
+      const {column = []} = me.$attrs
+
+      const cols = []
+      column.forEach(v => {
+        cols.push({
+          prop: v.prop,
+          label: v.label
+        })
+      })
+
+      const col = {
+        prop: '_filters',
+        width: 40,
+        renderHead: () => {
+          return (
+            <el-popover
+              placement="right"
+              width="400"
+              trigger="hover">
+              <el-checkbox-group v-model={me.filterField} onChange={me.filterChange}>
+                {
+                  cols.map(v => (
+                    <el-checkbox label={v.prop}>{v.label}</el-checkbox>
+                  ))
+                }
+              </el-checkbox-group>
+              <i slot="reference" class="el-icon-arrow-down el-icon--right"></i>
+            </el-popover>
+          )
+        }
+      }
+      return col
+    },
+    column () {
+      const me = this
+      let {column = []} = me.$attrs
+      const {radio, enableFilters, filterCol, radioCol} = me
+      const result = []
+
+      if (radio) {
+        result.push(radioCol)
+      }
+      result.push(...column)
+
+      if (enableFilters) {
+        result.push(filterCol)
+      }
+      return result
+    }
+  },
+  methods: {
+    // 清除选中效果
+    clearRadioValue () {
+      this.radioValue = null
+    },
+    // 设置默认选中
+    setRadioSelect (v) {
+      this.radioValue = v
+    },
+    filterChange (v) {
+      console.info(v)
+    }
+  },
+  components: {
+    'table-column': {
+      props: {
+        value: {
+          required: true,
+          type: Object,
+          default: () => ({})
+        }
+      },
+      render: function (h) {
+        const attrs = this.value
+
+        if (attrs.type) {
+          return (
+            <el-table-column {...{attrs}}/>
+          )
+        }
+
+        return (
+          <el-table-column
+            {...{attrs}}
+            scopedSlots={{
+              default: ({row, column, $index}) => {
+                return this.value.render ? this.value.render(h, {row, column, $index}) : row[column.property]
+              },
+              header: ({column, $index}) => {
+                return this.value.renderHead ? this.value.renderHead(h, {column, $index}) : column.label
+              }
+            }}>
+          </el-table-column>
+        )
+      }
+    }
+  }
+}
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,9 +2,68 @@
  * @Author: michael
  * @Date: 2019-12-31 10:58:05
  * @LastEditors  : michael
- * @LastEditTime : 2019-12-31 14:46:55
+ * @LastEditTime : 2020-01-02 15:05:04
  -->
 
 <template>
-  <div>home</div>
+  <div>home
+    <dntTable :column="columns7" :data="data6"/>
+  </div>
 </template>
+<script>
+import dntTable from '@/components/el-table'
+export default {
+  components: {dntTable},
+  data () {
+    return {
+      columns7: [
+        {
+          label: 'Name',
+          prop: 'name'
+        },
+        {
+          label: 'Age',
+          prop: 'age'
+        },
+        {
+          label: 'Address',
+          prop: 'address'
+        },
+        {
+          label: 'Action',
+          prop: 'action',
+          width: 150,
+          align: 'center',
+          render: (h, params) => {
+            return (
+              <el-button>jsx</el-button>
+            )
+          }
+        }
+      ],
+      data6: [
+        {
+          name: 'John Brown',
+          age: 18,
+          address: 'New York No. 1 Lake Park'
+        },
+        {
+          name: 'Jim Green',
+          age: 24,
+          address: 'London No. 1 Lake Park'
+        },
+        {
+          name: 'Joe Black',
+          age: 30,
+          address: 'Sydney No. 1 Lake Park'
+        },
+        {
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park'
+        }
+      ]
+    }
+  }
+}
+</script>
